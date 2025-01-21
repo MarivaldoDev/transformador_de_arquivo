e02cf910e4c846c06d703cc06c2fee977d0fec4f
MarivaldoDev <pedromarivaldo10@gmail.com>
2025-01-20 22:25:13 -0300

    :up:funções de conversão concluidas

--- functions/main.py
+++ functions/main.py
@@ -1,22 +1,63 @@
+from tkinter import messagebox
 from pdf2docx import Converter
-from docx import Document
+import win32com.client
 from docx2pdf import convert
+from PIL import Image
 
 
 def pdf_to_word(pdf_file, word_file):
     converter = Converter(pdf_file)
-    converter.convert(word_file, start=0, end=None)
+    try:
+        converter.convert(word_file, start=0, end=None)
+    except:
+       messagebox.showerror("Erro!", "Algo deu errado!")
+    else:
+        messagebox.showinfo("Sucesso!", "Arquivo convertido para docx")
+        converter.close()
     
-    converter.close()
     
-    print(f"Arquivo convertido para word: {word_file}")
-
-
 def word_to_pdf(word_file, pdf_file):
-   convert(word_file, pdf_file)
+   try:
+    convert(word_file, pdf_file)
+   except:
+      messagebox.showerror("Erro!", "Algo deu errado!")
+   else:
+       messagebox.showinfo("Sucesso!", "Arquivo convertido para pdf")
 
-   print(f"Arquivo convertido para PDF: {pdf_file}")
+
+def jpg_to_pdf(jpg_file, pdf_file):
+    image = Image.open(jpg_file)
+
+    try:
+        image.save(pdf_file)
+    except:
+        messagebox.showerror("Erro!", "Algo deu errado!")
+    else:
+        messagebox.showinfo("Sucesso!", "Arquivo convertido para pdf")
 
 
-# word_to_pdf("documents/grupo primeiro de maio.docx", "documents/teste.pdf")
-pdf_to_word("documents/profile.pdf", "documents/teste.docx")
+def excel_to_pdf(excel_file, pdf_file):
+    # Inicia o aplicativo Excel
+    excel = win32com.client.Dispatch("Excel.Application")
+    excel.Visible = False  # Não exibe o Excel durante a execução
+
+    # Abre o arquivo Excel
+    workbook = excel.Workbooks.Open(excel_file)
+
+    try:
+        # Exporta para PDF
+        workbook.ExportAsFixedFormat(
+            Type=0,  # 0 indica PDF
+            Filename=pdf_file,
+            Quality=0,  # Qualidade padrão
+            IncludeDocProperties=True,
+            IgnorePrintAreas=False,
+            OpenAfterPublish=False  # Não abre o PDF após salvar
+        )
+        messagebox.showinfo("Sucesso!", "Arquivo convertido para pdf")
+    except Exception as e:
+        print(f"Erro ao converter Excel para PDF: {e}")
+    finally:
+        # Fecha o workbook e encerra o Excel
+        workbook.Close(False)
+        excel.Quit()
--- interface.py
+++ interface.py
@@ -1,5 +1,8 @@
+import os
 from customtkinter import *
-
+from tkinter import filedialog
+from functions.main import *
+from PIL import Image
 
 
 class Conversor:
@@ -10,7 +13,6 @@ class Conversor:
         self.window.iconbitmap("images/logo.ico")
         self.window.resizable(False, False)
         self.options()
-        self.buttons_positions()
         self.window.mainloop()
 
 
@@ -22,7 +24,8 @@ class Conversor:
             text_color='white', 
             hover_color='#A52A2A',
             font=('Verdana', 18),
-            height=30
+            height=30,
+            command=self.window_pdf_to_docx
         )
 
         self.option2 = CTkButton(self.window, 
@@ -31,7 +34,8 @@ class Conversor:
             text_color='white', 
             hover_color='#00008B',
             font=('Verdana', 18),
-            height=30
+            height=30,
+            command=self.window_docx_to_pdf
         )
 
         self.option3 = CTkButton(self.window, 
@@ -41,7 +45,8 @@ class Conversor:
             hover_color='#2F4F4F',
             font=('Verdana', 18),
             height=30,
-            width=155
+            width=155,
+            command=self.window_jpg_to_pdf
         )
 
         self.option4 = CTkButton(self.window, 
@@ -51,20 +56,170 @@ class Conversor:
             hover_color='#006400',
             font=('Verdana', 18),
             height=30,
-            width=155
+            width=155,
+            command=self.window_xlsx_to_pdf
         )
 
 
-    def buttons_positions(self):
         self.option1.place(x=125, y=50)
         self.option2.place(x=125, y=100)
         self.option3.place(x=125, y=150)
         self.option4.place(x=125, y=200)
+        
+
+    def pdf_to_docx(self):
+        caminho_entrada = self.arquivo
+        caminho_saida = self.nome_arquivo.replace(".pdf", ".docx")
+        
+      
+        pdf_to_word(caminho_entrada, caminho_saida)
+        self.entrada_arquivo.delete(0, END)
+       
+
+    def docx_to_pdf(self):
+        caminho_entrada = self.arquivo
+        caminho_saida = caminho_entrada.replace(".docx", ".pdf")
+        
+      
+        word_to_pdf(caminho_entrada, caminho_saida)
+        self.entrada_arquivo.delete(0, END)
 
 
-    def window_pdf_to_word(self):
-        pass
+    def jpg_to_pdf(self):
+        caminho_entrada = self.arquivo
+        caminho_saida = caminho_entrada.replace(".jpg", ".pdf")
+        
+      
+        jpg_to_pdf(caminho_entrada, caminho_saida)
+        self.entrada_arquivo.delete(0, END)
 
+
+    def xlsx_to_pdf(self):
+        caminho_entrada = self.arquivo
+        caminho_saida = caminho_entrada.replace(".xlsx", ".pdf")
+        
+      
+        excel_to_pdf(caminho_entrada, caminho_saida)
+        self.entrada_arquivo.delete(0, END)
+
+
+    def escolher_arquivo(self):
+        self.arquivo = filedialog.askopenfilename(title="Escolha o arquivo")
+
+        if self.arquivo:
+            self.nome_arquivo = os.path.basename(self.arquivo)       
+            self.entrada_arquivo.insert(0, self.nome_arquivo)
+
+
+    def window_pdf_to_docx(self):
+        window = CTk()
+        window.title("PDF para DOCX")
+        window.geometry("400x300")
+
+        self.entrada_arquivo = CTkEntry(window, width=250, height=30, border_color='blue')
+        self.entrada_arquivo.place(x=70, y=60)
+
+        botao = CTkButton(window, 
+            text="Procurar arquivo",
+            command=self.escolher_arquivo,
+            width=150,
+            font=('Verdana', 16)
+        )
+        botao.place(x=120, y=120)
+
+        converter = CTkButton(window, text="Converter", 
+            font=('Verdana', 16), 
+            command=self.pdf_to_docx,
+            fg_color='#191970',
+            hover_color='#000080'
+        )
+        converter.place(x=120, y=250)
+
+        window.mainloop()
+
+
+    def window_docx_to_pdf(self):
+        window = CTk()
+        window.title("DOCX para PDF")
+        window.geometry("400x300")
+
+        self.entrada_arquivo = CTkEntry(window, width=250)
+        self.entrada_arquivo.place(x=70, y=60)
+
+        botao = CTkButton(window, 
+            text="Procurar arquivo",
+            command=self.escolher_arquivo,
+            width=150,
+            font=('Verdana', 16)
+        )
+        botao.place(x=120, y=120)
+
+        converter = CTkButton(window, text="Converter", 
+            font=('Verdana', 16), 
+            command=self.pdf_to_docx,
+            fg_color='#191970',
+            hover_color='#000080'
+        )
+        converter.place(x=120, y=250)
+
+
+        window.mainloop()
+
+
+    def window_jpg_to_pdf(self):
+        window = CTk()
+        window.title("JPG para PDF")
+        window.geometry("400x300")
+
+        self.entrada_arquivo = CTkEntry(window, width=250)
+        self.entrada_arquivo.place(x=70, y=60)
+
+        botao = CTkButton(window, 
+            text="Procurar arquivo",
+            command=self.escolher_arquivo,
+            width=150,
+            font=('Verdana', 16)
+        )
+        botao.place(x=120, y=120)
+
+        converter = CTkButton(window, text="Converter", 
+            font=('Verdana', 16), 
+            command=self.pdf_to_docx,
+            fg_color='#191970',
+            hover_color='#000080'
+        )
+        converter.place(x=120, y=250)
+
+
+        window.mainloop()
+
+
+    def window_xlsx_to_pdf(self):
+        window = CTk()
+        window.title("Excel para PDF")
+        window.geometry("400x300")
+
+        self.entrada_arquivo = CTkEntry(window, width=250)
+        self.entrada_arquivo.place(x=70, y=60)
+
+        botao = CTkButton(window, 
+            text="Procurar arquivo",
+            command=self.escolher_arquivo,
+            width=150,
+            font=('Verdana', 16)
+        )
+        botao.place(x=120, y=120)
+
+        converter = CTkButton(window, text="Converter", 
+            font=('Verdana', 16), 
+            command=self.pdf_to_docx,
+            fg_color='#191970',
+            hover_color='#000080'
+        )
+        converter.place(x=120, y=250)
+
+
+        window.mainloop()
 
 
 
